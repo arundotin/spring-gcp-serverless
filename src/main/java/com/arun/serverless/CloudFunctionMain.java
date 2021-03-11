403ab3f5a00d44e069625c8ed9eb81284bd1fa8a
Arun M <marunk20@ford.com>
2021-03-12 00:42:41 +0530

    GCP Spring Cloud Functions

---
+++ src/main/java/com/arun/serverless/CloudFunctionMain.java
@@ -0,0 +1,21 @@
+package com.arun.serverless;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Function;
+
+@SpringBootApplication
+public class CloudFunctionMain {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudFunctionMain.class, args);
+    }
+
+    @Bean
+    public Function<String, String> uppercase() {
+        return value -> value.toUpperCase();
+    }
+
+}
